bd3912f19f3d74a1d03ad202f5ac24e53bfffa72
Jiseong <jpebs2016@gmail.com>
2018-12-07 14:04:49 +0000

    First test.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,2 +1,11 @@
 require 'fizzbuzz'
 
+describe FizzBuzz do
+  describe "checks if fizzbuzz" do
+    it "see if num is fizzbuzz" do
+      check_num = FizzBuzz.new
+
+      expect(check_num.fizz_buzz(3)).to eq("Fizz")
+    end
+  end
+end
