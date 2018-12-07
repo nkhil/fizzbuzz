b05ac3d656cd876cd5a1843fda8a9f0fb6d01f7f
Nikhil Vijayan <nkhilv@gmail.com>
2018-12-07 14:15:28 +0000

    added another test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,22 @@
 require 'fizzbuzz'
 
 describe FizzBuzz do
-  describe "checks if fizzbuzz" do
+  describe "checks if 3 is fizz" do
     it "see if num is fizzbuzz" do
       check_num = FizzBuzz.new
 
       expect(check_num.fizz_buzz(3)).to eq("Fizz")
     end
   end
+
+  describe "checks if 5 is buzz" do
+    it "should output buzz" do
+      check_num = FizzBuzz.new
+      expect(check_num.fizz_buzz(5)).to eq("Buzz")
+    end
+  end
+
+
 end
+
+
